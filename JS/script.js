1b3fe3caffb5b63403f35531fbe3fb9788cb22ab
Clarisse13@ <veroniceferos@gmail.com>
2026-08-12 10:46:57 -0300

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -43,3 +43,46 @@ campoTeclado.addEventListener('keyup', function(evento) {
     registrarLog("[TECLA LIBERADA] Teclado: Evento 'keyup' disparado!");
 
 });
+
+// Evento de formulário: input
+const meuFormulario = document.getElementById('meu-formulario');
+const campoInput = document.getElementById('campo-input');
+
+campoInput.addEventListener('input', function(evento) {
+    registrarLog(`[INPUT] Formulário: Evento 'input' disparado! -> Valor atual: "${campoInput.value}"`);
+});
+
+ 
+// Evento de formulário: submit (acesso ao evento de envio via id do formulário)
+// meuFormulário.addEventListener('submit', function(evento) {
+//        evento.preventDefailt(); // Evita o envio do formulário para fins de demonstração
+//        registrarLog("[SUBMIT] Formulário: Evento 'submit' disparado!");
+//});
+
+// Isso vai "funcionar", mas vai capturar apenas o clique no botão de envio, não o envio do formulário
+// em si. Para capturar o envio do forulário, é melhor usar o evento 'submit' no próprio formulário. 
+//const campoEnvio = document.getElementById('campo-envio');
+//meuFormulario.addEventListener('submit', function(evento) {
+    //evento.preventDefault(); // Evita o envio do formulário para fins de demonstração
+   // registrarLog("[SUBMIT] Formulário: Evento 'submit' disparado!");
+//});
+
+meuFormulario.addEventListener('submit', function(evento) {
+    evento.preventDefault(); // Impede o envio padrão do formulário
+    registrarLog("[SUBMIT] Formulário: Evento 'submit' disparado!");
+});
+
+
+
+
+const campoEnvio = document.getElementById('campo-envio');
+meuFormulario.addEventListener('submit', function(evento) {
+    evento.preventDefault(); // Evita o envio do formulário para fins de demonstração
+    registrarLog("[SUBMIT] Formulário: Evento 'submit' disparado!");
+});
+
+
+
+
+
+
